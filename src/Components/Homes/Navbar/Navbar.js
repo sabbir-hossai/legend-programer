89c9d6cb7d61ae0713d02ec14d5cb8d6ee9e315c
sabbir-hossai <sabbirab3@gmail.com>
2022-02-13 16:57:13 +0600

    navbar completed

--- src/Components/Homes/Navbar/Navbar.js
+++ src/Components/Homes/Navbar/Navbar.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <div className=' ' >
+            <nav className="navbar navbar-expand-lg navbar-expand-md navbar-light bg-light">
+                <div className="container">
+                    <a className="navbar-brand ml-5" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">dashboard</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">About</a>
+
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            {
+                                <button className="btn btn-outline-success" type="submit">Login</button>
+                                // <button className="btn btn-outline-success" type="submit">Log out</button>
+                            }
+
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div >
     );
 };
 
