e387a06cd50c86b4fa07530759a1da3cccf99d1b
sabbir-hossai <sabbirab3@gmail.com>
2022-02-14 13:41:19 +0600

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./Components/Abouts/About/About";
+import AddProduct from "./Components/Dashboards/AddProduct/AddProduct";
 import Dashboard from "./Components/Dashboards/Dashboard/Dashboard";
+import MakeAdmin from "./Components/Dashboards/MakeAdmin/MakeAdmin";
+import MyOrder from "./Components/Dashboards/MyOrder/MyOrder";
+import PaymentMethod from "./Components/Dashboards/PayMentMethod/PaymentMethod";
+import Reviews from "./Components/Dashboards/reviews/Reviews";
 import Home from "./Components/Homes/Home/Home";
 // import { render } from "react-dom";
 
@@ -10,43 +15,24 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* PrivateRoute  */}
-
-          {/* <Route path="/appointment" element={
-            <PrivateRoute>
-              <Appointment />
-            </PrivateRoute>}>
-          </Route>
-          <Route path="/dashboard" element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>}> */}
-
-          --------
-
-          {/* nestedRoute  */}
-
-          {/* <Route exact path="/dashboard" element={<DashboardHome></DashboardHome>}>
-            </Route>
-            <Route path={`/dashboard/payment/:appointmentId`} element={<Payment></Payment>}>
-
-            </Route>
-            <Route path={`/dashboard/makeAdmin`} element={<AdminRoute>
-              <MakeAdmin></MakeAdmin>
-            </AdminRoute>}>
-            </Route>
-            <Route path={`/dashboard/addDoctor`} element={<AdminRoute>
-              <AddDoctor></AddDoctor>
-            </AdminRoute>}>
-            </Route>
-          </Route> */}
           {/* normal route */}
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
+          {/* nestedRoute  */}
+
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="makeAdmin" element={<MakeAdmin></MakeAdmin>} />
+            <Route path="addProduct" element={<AddProduct></AddProduct>} />
+            <Route path="payMentMethod" element={<PaymentMethod></PaymentMethod>} />
+            <Route path="MyOrders" element={<MyOrder></MyOrder>} />
+            <Route path="Reviews" element={<Reviews></Reviews>} />
+            {/* <Route path=":id" element={<ProductDisplay />} /> */}
+          </Route>
         </Routes>
       </BrowserRouter>
-    </div>
+    </div >
   );
 }
 
--- src/Components/Dashboards/Dashboard/Dashboard.js
+++ src/Components/Dashboards/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import DashboardItems from '../DashbordItems/DashboardItems';
+import DashboardItems from '../reviews/Reviews';
 import MyOrder from '../MyOrder/MyOrder';
 import './Dashboard.css';
+import {
+    Link,
+    Outlet,
+
+} from "react-router-dom";
 
 const Dashboard = () => {
     return (
@@ -10,8 +15,16 @@ const Dashboard = () => {
             <div className='container'>
                 <div class="row  ">
 
-                    <div className=" col-lg-3   DashItem "> <DashboardItems></DashboardItems></div>
-                    <div class=" col-9"> <MyOrder></MyOrder></div>
+                    <div className=" col-lg-3   DashItem ">
+                        <Link to="/Dashboard/makeAdmin"><h6>Make Admin</h6> </Link>
+                        <Link to="/Dashboard/addProduct"> <h6>Add Product</h6> </Link>
+                        <Link to="/Dashboard/payMentMethod"><h6>Pay</h6></Link>
+                        <Link to="/Dashboard/MyOrders"><h6>My Order </h6> </Link>
+                        <Link to="/Dashboard/Reviews" ><h6>Reviews</h6></Link>
+                    </div>
+                    <div class=" col-9">
+                        <Outlet />
+                    </div>
                 </div>
             </div>
 
@@ -21,3 +34,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
